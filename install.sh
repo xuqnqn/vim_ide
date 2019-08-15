3486ee4fa10b416588b7a5a28f1621a945cefaf0
xuqnqn <xuqnqn@qq.com>
2019-08-15 18:21:02 +0800

    fix color error and syntax error

--- install.sh
+++ install.sh
@@ -14,5 +14,17 @@ mkdir -p ~/bin/vim_bin
 cp -r src_bin/bin/* ~/bin/vim_bin
 cp sourceInsight.sh ~/bin/vim_bin/bin/
 echo export PATH=~/bin/vim_bin/bin:$PATH >> ~/.bashrc
+source ~/.bashrc
 echo ===== version vim ide "$VERSION", by xuqnqn, `date` ========= > ~/bin/vim_bin/version
+
+#for syntax error
+cd /home/xuqnqn
+mkdir -p work/vim_src/vim_bin/share/vim/
+cp -r .vim/src_bin/bin/share/vim/vim74/syntax/ work/vim_src/vim_bin/share/vim/
+
+##for color error
+#cd /home/xuqnqn/.vim
+#cp -r ./src_bin/bin/share/vim/vim74/colors .
+
+
 echo Success!
